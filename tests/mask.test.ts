08d8881b37229e57ae93aada47c534a11ad1f0f8
ninest <parth.kabra@gmail.com>
2020-11-20 21:26:08 +0800

    Improve masking tests

--- tests/mask.test.ts
+++ tests/mask.test.ts
@@ -1,16 +1,10 @@
 import { maskNric } from '../src/index';
 
-test('S0000002G masks to 002G', () => {
-  expect(maskNric('S0000002G')).toBe('002G');
-});
-
-test('s0000007h masks to 007H', () => {
-  expect(maskNric('s0000007H')).toBe('007H');
-});
-
-test('S0000002G with first letter masks to S002G', () => {
-  expect(maskNric('S0000002G', true)).toBe('S002G');
-});
-test('s0000007h with first letter masks to S007H', () => {
-  expect(maskNric('s0000007H', true)).toBe('S007H');
+test.each([
+  ['S0000002G', '002G', false],
+  ['s0000007h', '007H', false],
+  ['S0000002G', 'S002G', true],
+  ['s0000007h', 'S007H', true],
+])('Validating NRICs', (nric, maskedNric, firstLetter) => {
+  expect(maskNric(nric, firstLetter)).toBe(maskedNric);
 });
